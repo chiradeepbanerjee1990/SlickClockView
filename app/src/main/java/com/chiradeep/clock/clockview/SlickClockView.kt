94c7fd11d138aa311af8a56b950eb1a7ed4cb91e
Chiradeep <chiradeep.banerjee@walmartlabs.com>
2020-02-11 14:23:56 +0530

    Clock view hour and min hand added | Clock view pointer added

--- app/src/main/java/com/chiradeep/clock/clockview/SlickClockView.kt
+++ app/src/main/java/com/chiradeep/clock/clockview/SlickClockView.kt
@@ -2,14 +2,24 @@ package com.chiradeep.clock.clockview
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
 import com.chiradeep.clock.R
+import kotlin.math.cos
+import kotlin.math.sin
 
 
 class SlickClockView  :
     View{
+
+    private val numbers = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+    private val clockText = "|"
+    private var rect = Rect()
+    private var padding = 45
 
     constructor(context: Context?) : super(context,null) {
         init(null)
@@ -23,20 +33,37 @@ class SlickClockView  :
         init(attr)
     }
 
-    private var outerCircleRadius : Float = 200f
-    private var innerCircleRadius : Float = 198.5f
+    private var outerCircleRadius : Float = 0f
+    private var innerCircleRadius : Float = outerCircleRadius
+
+    private var elevationRadius : Float = 0f
 
     private var outerCirclePaint : Paint = Paint()
     private var innerCirclePaint : Paint = Paint()
 
+    var paint = Paint()
+
+
+
+
     fun init(attr: AttributeSet?=null){
+
+        var tf = context.obtainStyledAttributes(attr,R.styleable.SlickClockView)
+
+        outerCircleRadius = tf.getFloat(R.styleable.SlickClockView_outerradius,0f)
+        elevationRadius = tf.getFloat(R.styleable.SlickClockView_shadowradius,0f)
+        innerCircleRadius = outerCircleRadius - 1.5f
+
+
 
         outerCirclePaint.isAntiAlias = true
         outerCirclePaint.strokeWidth = 3f
         outerCirclePaint.color = context.resources.getColor(R.color.clockcolor)
         outerCirclePaint.style = Paint.Style.FILL
-        outerCirclePaint.setShadowLayer(10f,0f,0f,context.resources.getColor(R.color.clockcolor))
+        outerCirclePaint.setShadowLayer(elevationRadius,0f,0f,context.resources.getColor(R.color.clockcolor))
         setLayerType(LAYER_TYPE_SOFTWARE,outerCirclePaint)
+
+
 
         innerCirclePaint.isAntiAlias = true
         innerCirclePaint.strokeWidth = 3f
@@ -44,6 +71,17 @@ class SlickClockView  :
         innerCirclePaint.style = Paint.Style.FILL
 
 
+
+        paint.color = Color.BLACK
+        paint.strokeWidth = 8f
+        paint.style = Paint.Style.FILL
+        paint.isAntiAlias = true
+
+
+
+
+
+        tf.recycle()
 
     }
 
@@ -53,6 +91,8 @@ class SlickClockView  :
 
         var viewHeight = height/2
         var viewWidth = width/2
+
+        canvas?.save()
 
         canvas?.drawCircle(viewWidth.toFloat(),
             viewHeight.toFloat(),
@@ -64,5 +104,27 @@ class SlickClockView  :
             innerCircleRadius*2,
             innerCirclePaint)
 
+        canvas?.drawCircle(viewWidth.toFloat(),
+            viewHeight.toFloat(),
+            8f,
+            paint)
+
+
+        paint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,15f,context.resources.displayMetrics)
+
+        for (number in numbers) {
+            paint.getTextBounds(clockText, 0, clockText.length, rect)
+            val angle = Math.PI / 6 * (number - 3)
+            val x = (width/2  + cos(angle) * (innerCircleRadius*2 - padding)- rect.width() / 2).toFloat()
+            val y = (height/2  + sin(angle) * (innerCircleRadius*2 - padding) + rect.height() / 2).toFloat()
+            canvas?.drawText(clockText, x, y, paint)
+
+        }
+
+
     }
+
+
+
+
 }
